c7ca4b81a8177e8df13e3d132a9ea79c322c6cb7
granat-s <anatolij.solonenko@proton.me>
2023-11-29 12:56:15 +0200

    Adding Linux folder

---
+++ Trainee/Linux/shell_script/test.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+if [ "$X" -lt "0" ]
+then
+	echo "X is less than zero"
+fi
+if [ "$X" -gt "0" ]; then
+	echo "X is more than zero"
+fi
+[ "$X" -le "0" ] && \
+	echo "X is less than or equal to zero"
+[ "$X" -ge "0" ] && \
+	echo "X is more than or equal to zero"
+[ "$X" = 0 ] && \
+	echo "X is the string or number"
+[ "$X" = "hello" ] && \
+	echo "X matches the string \"hello\""
+[X "$X" != "hello" ] && \
+	echo "X is not hte string \"hello\""
+[ -n "$X" ] && \
+	echo "X us the path or a real file" || \
+	echo "No such file: $X"
+[ -x "$X" ] && \
+	echo "X is the path of an executable file"
+[ "$X" -nt "/ect/passwd" ] && \
+	echo "X is a file which is newer than /etc/passwd"
+	
+
