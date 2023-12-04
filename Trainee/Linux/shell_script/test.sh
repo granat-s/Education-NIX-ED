f26d8bf95d2fc4f99d8ce13bf18b50791dec2bf3
granat-s <anatolij.solonenko@proton.me>
2023-12-04 14:49:20 +0200

    Adding more .sh exercises

--- Trainee/Linux/shell_script/test.sh
+++ Trainee/Linux/shell_script/test.sh
@@ -9,7 +9,7 @@ fi
 [ "$X" -le "0" ] && \
 	echo "X is less than or equal to zero"
 [ "$X" -ge "0" ] && \
-	echo "X is more than or equal to zero"
+echo "X is more than or equal to zero"
 [ "$X" = 0 ] && \
 	echo "X is the string or number"
 [ "$X" = "hello" ] && \
